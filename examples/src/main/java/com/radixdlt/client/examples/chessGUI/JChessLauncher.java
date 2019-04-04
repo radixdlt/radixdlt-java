f117fa4c53392975d57ed3b4715bc80e5fdd8315
Greg <greg@Gregs-MacBook-Pro.local>
2019-04-04 14:07:49 +0100

    gui app dependency added

---
+++ examples/src/main/java/com/radixdlt/client/examples/chessGUI/JChessLauncher.java
@@ -0,0 +1,9 @@
+package com.radixdlt.client.examples.chessGUI;
+
+import pl.art.lach.mateusz.javaopenchess.JChessApp;
+
+public class JChessLauncher {
+    public static void main(String[] args) {
+        JChessApp.main(args);
+    }
+}
