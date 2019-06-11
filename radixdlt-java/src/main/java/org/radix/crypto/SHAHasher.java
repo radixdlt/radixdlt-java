f7d7d48042100bea01ce0248312705c4f5d050c0
Dan <dan@radixdlt.com>
2019-06-11 17:47:52 +0100

    Converted synchronized digesters to ThreadLocal instances

--- radixdlt-java/src/main/java/org/radix/crypto/SHAHasher.java
+++ radixdlt-java/src/main/java/org/radix/crypto/SHAHasher.java
@@ -7,21 +7,26 @@ import java.security.NoSuchProviderException;
 class SHAHasher implements Hasher {
 	// Note that default provide around 20-25% faster than Bouncy Castle.
 	// See jmh/org.radix.benchmark.HashBenchmark
-	private final MessageDigest hash256DigesterInner = getDigester("SHA-512", null);
-	private final MessageDigest hash256DigesterOuter = getDigester("SHA-256", null);
-	private final MessageDigest hash512Digester      = getDigester("SHA-512", null);
+	// Note that default provide around 20-25% faster than Bouncy Castle.
+	// See jmh/org.radix.benchmark.HashBenchmark
+	private final ThreadLocal<MessageDigest> hash256DigesterInner = ThreadLocal.withInitial(() -> getDigester("SHA-512", null));
+	private final ThreadLocal<MessageDigest> hash256DigesterOuter = ThreadLocal.withInitial(() -> getDigester("SHA-256", null));
+	private final ThreadLocal<MessageDigest> hash512Digester = ThreadLocal.withInitial(() -> getDigester("SHA-512", null));
 
 	SHAHasher() {
 	}
-
+	
 	@Override
 	public byte[] hash256Raw(byte[] data) {
-		synchronized (hash256DigesterOuter) {
-			hash256DigesterOuter.reset();
-			hash256DigesterOuter.update(data);
-			return hash256DigesterOuter.digest();
-		}
+		// Here we use SHA-256(SHA-512(data)) to avoid length-extension attack
+		final MessageDigest hash256DigesterOuterLocal = hash256DigesterOuter.get();
+		final MessageDigest hash256DigesterInnerLocal = hash256DigesterInner.get();
+		hash256DigesterOuterLocal.reset();
+		hash256DigesterInnerLocal.reset();
+		hash256DigesterInnerLocal.update(data);
+		return hash256DigesterOuterLocal.digest(hash256DigesterInnerLocal.digest());
 	}
+
 
 	@Override
 	public byte[] hash256(byte[] data) {
@@ -31,33 +36,32 @@ class SHAHasher implements Hasher {
 	@Override
 	public byte[] hash256(byte[] data, int offset, int length) {
 		// Here we use SHA-256(SHA-512(data)) to avoid length-extension attack
-		synchronized (hash256DigesterOuter) {
-			hash256DigesterOuter.reset();
-			hash256DigesterInner.reset();
-			hash256DigesterInner.update(data, offset, length);
-			return hash256DigesterOuter.digest(hash256DigesterInner.digest());
-		}
+		final MessageDigest hash256DigesterOuterLocal = hash256DigesterOuter.get();
+		final MessageDigest hash256DigesterInnerLocal = hash256DigesterInner.get();
+		hash256DigesterOuterLocal.reset();
+		hash256DigesterInnerLocal.reset();
+		hash256DigesterInnerLocal.update(data, offset, length);
+		return hash256DigesterOuterLocal.digest(hash256DigesterInnerLocal.digest());
 	}
 
 	@Override
 	public byte[] hash256(byte[] data0, byte[] data1) {
 		// Here we use SHA-256(SHA-512(data0 || data1)) to avoid length-extension attack
-		synchronized (hash256DigesterOuter) {
-			hash256DigesterInner.reset();
-			hash256DigesterOuter.reset();
-			hash256DigesterInner.update(data0);
-			return hash256DigesterOuter.digest(hash256DigesterInner.digest(data1));
-		}
+		final MessageDigest hash256DigesterInnerLocal = hash256DigesterInner.get();
+		final MessageDigest hash256DigesterOuterLocal = hash256DigesterOuter.get();
+		hash256DigesterInnerLocal.reset();
+		hash256DigesterOuterLocal.reset();
+		hash256DigesterInnerLocal.update(data0);
+		return hash256DigesterOuterLocal.digest(hash256DigesterInnerLocal.digest(data1));
 	}
 
 	@Override
 	public byte[] hash512(byte[] data, int offset, int length) {
 		// Here we use SHA-512(SHA-512(data0 || data1)) to avoid length-extension attack
-		synchronized (hash512Digester) {
-			hash512Digester.reset();
-			hash512Digester.update(data, offset, length);
-			return hash512Digester.digest(hash512Digester.digest());
-		}
+		final MessageDigest hash512DigesterLocal = hash512Digester.get();
+		hash512DigesterLocal.reset();
+		hash512DigesterLocal.update(data, offset, length);
+		return hash512DigesterLocal.digest(hash512DigesterLocal.digest());
 	}
 
 	private static MessageDigest getDigester(String algorithm, String provider) {
